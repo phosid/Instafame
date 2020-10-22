b975fca4a4c0ee5fe1ca9505ed7896fd0365d7e1
Sidney Pho <phosidney@gmail.com>
2020-10-22 16:54:16 -0500

    write to database functionality

--- instafame/CameraViewController.swift
+++ instafame/CameraViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import AlamofireImage
+import Parse
 class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
@@ -19,6 +20,23 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
     }
     
     @IBAction func onSubmit(_ sender: Any) {
+        
+        let post = PFObject(className: "Posts")
+        
+        post["caption"] = commentField.text!
+        post["author"] = PFUser.current()!
+        let imageData = imageView.image!.pngData()
+        let file = PFFileObject(data: imageData!)
+        post["image"] = file
+        post.saveInBackground { (success, error) in
+            if success {
+                self.dismiss(animated: true, completion: nil)
+                print("saved!")
+            } else {
+                print("error!")
+            }
+        }
+        
     }
     
     @IBAction func onCameraButton(_ sender: Any) {
@@ -38,8 +56,9 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
         let image = info[.editedImage] as! UIImage
         
         let size = CGSize(width: 300, height: 300)
-        let scaledImage = image.af_imageScaled(to: size)
+        let scaledImage = image.af.imageScaled(to: size)
         imageView.image = scaledImage
+        dismiss(animated: true, completion: nil)
     }
     
     /*
